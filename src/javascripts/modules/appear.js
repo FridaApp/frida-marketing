d09802df37e0d7adca9d3a610b6e4d8c1140a3bf
Célia Dagorne <celia.dagorne@hotmail.fr>
2018-06-10 22:43:00 +0200

    navigation balises

--- src/javascripts/modules/appear.js
+++ src/javascripts/modules/appear.js
@@ -1,28 +1,28 @@
 appear({
   init: function init(){
-    console.log('dom is ready');
+    console.log('dom is ready')
   },
   elements: function elements(){
-    return document.getElementsByClassName('card');
+    return document.getElementsByClassName('card')
   },
-  appear: function appear(el){
+  appear: function appear(el) {
     el.classList.add('card--appeared')
   },
   bounds: -100,
   reappear: false
-});
+})
 
 appear({
   init: function init(){
-    console.log('dom is ready');
+    console.log('dom is ready')
   },
   elements: function elements(){
     // work with all elements with the class "track"
-    return document.getElementsByClassName('card--workflow');
+    return document.getElementsByClassName('card--workflow')
   },
   appear: function appear(el){
     el.classList.add('card-workflow--appeared')
   },
   bounds: -200,
   reappear: false
-});
+})
