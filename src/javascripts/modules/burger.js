032ff4dc8b02905daeea50479a6557e81400c26f
Célia Dagorne <celia.dagorne@hotmail.fr>
2018-06-10 22:05:02 +0200

    burger fixed

--- src/javascripts/modules/burger.js
+++ src/javascripts/modules/burger.js
@@ -1,13 +1,14 @@
-burger = document.getElementsByClassName('burger')[0]
-burger_wrap = document.getElementsByClassName('burger-wrap')[0]
-mobileMenu = document.getElementsByClassName('mobileMenu')[0]
-
-burger.addEventListener('click', function () {
-  burger.classList.toggle('active')
-  document.body.classList.toggle('lock')
-  mobileMenu.classList.toggle('open')
-  console.log('hellloo')
+document.addEventListener('DOMContentLoaded', () => {
+  burger = document.getElementsByClassName('burger')[0]
+  burger_wrap = document.getElementsByClassName('burger-wrap')[0]
+  mobileMenu = document.getElementsByClassName('mobileMenu')[0]
+  
+  burger.addEventListener('click', function () {
+    burger.classList.toggle('active')
+    document.body.classList.toggle('lock')
+    mobileMenu.classList.toggle('open')
+    console.log('hellloo')
+  })
+  
+  console.log('hey')
 })
-
-console.log('hey')
-
