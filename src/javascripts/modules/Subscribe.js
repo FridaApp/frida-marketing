03ba12600d39794e6f67db2d24fb8713970fe3f8
Louis <louis.amiot92@gmail.com>
2018-06-13 11:45:54 +0200

    Some fixes

--- src/javascripts/modules/Subscribe.js
+++ src/javascripts/modules/Subscribe.js
@@ -1,46 +1,48 @@
 const SCRIPT_URL =
-    'https://script.google.com/macros/s/AKfycbx-7W6M3_ebHLesrrlR-R9xMjk4OEFu7BlhLRJAQ54p7dFuXmkz/exec'
+  'https://script.google.com/macros/s/AKfycbx-7W6M3_ebHLesrrlR-R9xMjk4OEFu7BlhLRJAQ54p7dFuXmkz/exec'
 
 class Subscribe {
-    constructor() {
-        this.$form = document.getElementById('subscribe-form')
-        this.$email = document.getElementById('email-input')
-        this.$button = document.querySelector('.subscribe__submit')
-        this.$subscribed = document.querySelector('.subscribe__subscribed')
-        this.init()
+  constructor() {
+    this.$form = document.getElementById('subscribe-form')
+    this.$email = document.getElementById('email-input')
+    this.$button = document.querySelector('.subscribe__submit')
+    this.$subscribed = document.querySelector('.subscribe__subscribed')
+    this.init()
+  }
+
+  init() {
+    if (this.$form) {
+      this.$form.addEventListener('submit', e => {
+        this.handleFormSubmit(e)
+      })
     }
 
-    init() {
-        this.$form.addEventListener('submit', e => {
-            this.handleFormSubmit(e)
-    })
-
-        if (localStorage.getItem('subscribed')) {
-            this.$subscribed.style.display = 'block'
-            this.$form.style.display = 'none'
-        }
+    if (localStorage.getItem('subscribed')) {
+      this.$subscribed.style.display = 'block'
+      this.$form.style.display = 'none'
     }
+  }
 
-    handleFormSubmit(e) {
-        e.preventDefault()
-        this.$button.setAttribute('disabled', 'disabled')
-        const url = new URL(SCRIPT_URL)
-        const params = { email: this.$email.value, timestamp: Date.now() }
-        Object.keys(params).forEach(key =>
-        url.searchParams.append(key, params[key])
+  handleFormSubmit(e) {
+    e.preventDefault()
+    this.$button.setAttribute('disabled', 'disabled')
+    const url = new URL(SCRIPT_URL)
+    const params = { email: this.$email.value, timestamp: Date.now() }
+    Object.keys(params).forEach(key =>
+      url.searchParams.append(key, params[key])
     )
-        console.log(url)
-        fetch(url)
-            .then(data => data.text())
-    .then(text => {
-            localStorage.setItem('subscribed', 'true')
+    console.log(url)
+    fetch(url)
+      .then(data => data.text())
+      .then(text => {
+        localStorage.setItem('subscribed', 'true')
         this.$subscribed.style.display = 'block'
         this.$form.style.display = 'none'
-    })
-    .catch(function(error) {
-            console.log('request failed', error)
-        })
-    }
+      })
+      .catch(function(error) {
+        console.log('request failed', error)
+      })
+  }
 }
 
 export default Subscribe
